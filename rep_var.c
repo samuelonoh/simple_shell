2dd96254f8f9a3d226db491688272c0a3f1b72fa
ONOH SAMUEL <samuelonoh12345@gmail.com>
2022-11-09 15:22:47 +0100

    task done

--- rep_var.c
+++ rep_var.c
@@ -65,7 +65,16 @@ int check_vars(r_var **h, char *in, char *st, data_shell *data)
 				add_rvar_node(h, 2, st, lst), i++;
 			else if (in[i + 1] == '$')
 				add_rvar_node(h, 2, data->pid, lpd), i++;
-			
+			else if (in[i + 1] == '\n')
+				add_rvar_node(h, 0, NULL, 0);
+			else if (in[i + 1] == '\0')
+				add_rvar_node(h, 0, NULL, 0);
+			else if (in[i + 1] == ' ')
+				add_rvar_node(h, 0, NULL, 0);
+			else if (in[i + 1] == '\t')
+				add_rvar_node(h, 0, NULL, 0);
+			else if (in[i + 1] == ';')
+				add_rvar_node(h, 0, NULL, 0);
 			else
 				check_env(h, in + i, data);
 		}
