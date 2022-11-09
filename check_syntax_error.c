5a464503b3647cf1d4cfd6e5dec112a37d52c4d1
ONOH SAMUEL <samuelonoh12345@gmail.com>
2022-11-09 16:04:16 +0100

    char

--- check_syntax_error.c
+++ check_syntax_error.c
@@ -46,7 +46,7 @@ int error_sep_op(char *input, int i, char last)
 
 		if (last == '|')
 		{
-			count =char(input, 0);
+			count = repeated_char(input, 0);
 			if (count == 0 || count > 1)
 				return (i);
 		}
