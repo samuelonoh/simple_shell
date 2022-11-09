6f7df04f0c80bea4bf637ba2662da95cf29cb270
ONOH SAMUEL <samuelonoh12345@gmail.com>
2022-11-09 15:16:36 +0100

    hmm... done

--- aux_help2.c
+++ aux_help2.c
@@ -9,7 +9,10 @@ void aux_help(void)
 	char *help = "help: help [-dms] [pattern ...]\n";
 
 	write(STDOUT_FILENO, help, _strlen(help));
-	
+	help = "\tDisplay information about builtin commands.\n ";
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "Displays brief summaries of builtin commands.\n";
+	write(STDOUT_FILENO, help, _strlen(help));
 }
 /**
  * aux_help_alias - Help information for the builtin alias.
